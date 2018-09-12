53fe6344d563fda9050cf521edd726696f2d11a6
Fabio Teixeira <fabio1079@gmail.com>
2018-09-12 08:05:57 -0300

    Remove dead code

--- tracker/fetcher.py
+++ tracker/fetcher.py
@@ -17,50 +17,6 @@ MY_ALT = "0"
 SATELLITE_URI = f"{BASE_URI}/satellite/positions/{ID}/{MY_LAT}/{MY_LONG}/{MY_ALT}/1/&apiKey={API_KEY}"
 
 
-class Satellite:
-    __slots__ = 'satid', 'satname', 'satlatitude', 'satlongitude', 'sataltitude'
-
-    def __init__(self):
-        self.satid = None
-        self.satname = None
-        self.satlatitude = None
-        self.satlongitude = None
-        self.sataltitude = None
-
-    def set_data(self, info, positions):
-        self.satid = info['satid']
-        self.satname = info['satname']
-        self.satlatitude = positions[0]['satlatitude']
-        self.satlongitude = positions[0]['satlongitude']
-        self.sataltitude = positions[0]['sataltitude']
-
-    def get_data(self):
-        data = {
-            "satid": self.satid,
-            "satname": self.satname,
-            "satlatitude": self.satlatitude,
-            "satlongitude": self.satlongitude,
-            "sataltitude": self.sataltitude,
-        }
-
-        return data
-
-
-satellite_data = Satellite()
-
-del Satellite  # satellite_data will be the only instance of Satellite
-
-
-def get_satellite(satellite):
-    with request.urlopen(SATELLITE_URI) as response:
-        data = json.loads(response.read())
-
-        try:
-            satellite.set_data(data['info'], data['positions'])
-        except:
-            print("Could not get satellite data")
-
-
 class Tracker:
     def __init__(self, uri):
         self.API_URI = uri
